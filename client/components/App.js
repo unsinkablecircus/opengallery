66bc7c0d1e4ea75fa690d2f619abe519e3c8870b
Megan Ring <meganbethring@gmail.com>
2016-03-10 23:21:51 -0800

    add some test content to verify webpack dev server

---
+++ client/components/App.js
@@ -0,0 +1,30 @@
+//Code that starts app goes here
+//React modules
+const React = require('react');
+const ReactDOM = require('react-dom');
+// const { Router, Route, Link, IndexRoute, hashHistory, RouterContext } = require('react-router');
+
+//import components here
+// const Nav = require('./nav/nav');
+
+const App = class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  render () {
+    return (
+    <div className='app-shell'>
+      <div className=''>
+        <div className=''>
+          <h1>{Hello, World!}</h1>
+        </div>
+      </div>
+    </div>
+    );
+  }
+
+};
+
+ReactDOM.render(( <App />), document.getElementById('app'));
