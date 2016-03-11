a30cc21115abc07b8f09c3fcf9de732818e25969
Megan Ring <meganbethring@gmail.com>
2016-03-10 23:01:12 -0800

    update entry point and loaders

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 var outputPath = __dirname + '/client';
 
 module.exports = {
-    entry: "./entry.js",
+    entry: "./../client/components/App.js",
     output: {
         path: outputPath,
         filename: "app.js"
@@ -13,8 +13,8 @@ module.exports = {
     module: {
         loaders: [
             { 
-                test: /\.css$/, 
-                loader: "style!css",
+                test: /\.jsx?$/, 
+                loader: 'babel-loader',
                 exclude: [/node_modules/, /typings/],
                 query: {
                     presets: ['es2015', 'react']
