c8413da1d912e8d7b108e769046d9b283e070d06
Daniel Wong <danielwong2268@gmail.com>
2016-03-11 11:21:51 -0800

    Add basic outlines to signin/signup routes

--- server/config/router.js
+++ server/config/router.js
@@ -1,6 +1,10 @@
+var helpers = require('./config/helpers.js');
 // insert controllers here
+var UserController = require('./users/userController.js');
 
 
 module.exports = function (app, express) {
   // insert routes here
+  app.post('/user/signin', UserController.signIn);
+  app.post('/user/signup', UserController.signUp);
 }
