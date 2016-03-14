3a1f34730f47f5a14c188efd63182f69b7158c16
John Domingo <johnwill.domingo@gmail.com>
2016-03-14 12:13:48 -0700

    Clean up comments and unnecessary modules

--- server/config/router.js
+++ server/config/router.js
@@ -1,12 +1,8 @@
-var path = require('path');
-
 var helpers = require('./helpers.js');
-// insert controllers here
+
 var UserController = require('../user/userController.js');
 
-
 module.exports = function (app, express) {
-  // insert routes here
   app.post('/user/signIn', UserController.signIn);
   app.post('/user/signUp', UserController.signUp);
 }
