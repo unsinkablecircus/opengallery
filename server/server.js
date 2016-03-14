86a46d52c1cb411fd164f2db34d6056e84e2dbf1
Evan Chen <evanwchen@gmail.com>
2016-03-14 11:30:42 -0700

    Switch to using process.env for storing postgresql passwords

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var AWS = require('aws-sdk');
-var config = require('./config/config.js');
 
 // adding connection to postgresql on main server.js for now, but we will move to separate models when we are ready
 var pg = require('pg');
 var connectionInfo = {
   host: 'opengallery.cbxmygjagdjr.us-west-1.rds.amazonaws.com',
   port: '5432',
-  user: config.postgresqlUser,
-  password: config.postgresqlPw,
+  user: process.env.AWS_POSTGRESQL_USERNAME,
+  password: process.env.AWS_POSTGRESQL_PW,
   database: 'opengallery'
 };
 
@@ -23,14 +22,11 @@ AWS.config.update({region: 'us-west-1'});
 require('./config/middleware.js')(app, express);
 require('./config/router.js')(app, express);
 
-/* example of connecting to postgresql database below: 
-// connection to postgresql db; will move to models later
-
-var client = new pg.Client(connectionInfo);
-client.connect();
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', function() { client.end(); });
-*/
+// example of connecting to postgresql database below (will move to models later):
+// var client = new pg.Client(connectionInfo);
+// client.connect();
+// var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+// query.on('end', function() { client.end(); });
 
 // listen to port
 var port = Number(process.env.PORT || 8000);
