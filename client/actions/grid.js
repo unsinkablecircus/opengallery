f4655c4af110c7eda34e352a8e3f95877a888344
John Domingo <johnwill.domingo@gmail.com>
2016-03-14 17:34:19 -0700

    Define actions related to reloading grid data

---
+++ client/actions/grid.js
@@ -0,0 +1,59 @@
+export const GRID_REQUEST = 'GRID_REQUEST'
+export const GRID_SUCCESS = 'GRID_SUCCESS'
+export const GRID_FAILURE = 'GRID_FAILURE'
+
+function requestGridData(tags) {
+  return {
+    type: GRID_REQUEST,
+    fetching: true
+    success: false
+    tags
+  }
+}
+
+function receiveGridData(data) {
+  return {
+    type: GRID_SUCCESS,
+    fetching: false
+    success: true
+    data
+  }
+}
+
+function catchGridData(error) {
+  return {
+    type: GRID_FAILURE,
+    fetching: false
+    success: false
+    error
+  }
+}
+
+export function loadGrid(tags) {
+  let params = {
+    method: 'GET',
+    query: { tags: tags.join('+') },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  }
+
+  return dispatch => {
+    // Dispatch requestGridData to kickoff the call to network API
+    dispatch(requestGridData(tags))
+
+    return fetch('/api/media', params)
+    .then(response => {
+      if (response.status >= 400) {
+        dispatch(catchGridData(data.message))
+        return Promise.reject(data)
+      }
+      return response.json()
+    })
+    .then(function(gridData) {
+      dispatch(receiveGridData(gridData))
+    })
+    .catch(err => {
+      console.error(`Network failure prevented data retrieval: ${err}`)
+      throw new Error(`Network failure prevented data retrieval: ${err}`)
+    })
+  }
+}
