6d50b27121d3e073770235c0e4342cd37053337f
Megan Ring <meganbethring@gmail.com>
2016-03-15 15:21:24 -0700

    Init upload functions

--- server/controllers/media.js
+++ server/controllers/media.js
@@ -1,3 +1,12 @@
 const pg import ('../db/database')
 const s3 import ('../s3/s3')
 
+exports.uploadPhoto = function (req, res) {
+  uploadToPG()
+  uploadToS3()
+  updatePGid()
+};
+
+uploadToPG = function (req, res, next) {};
+uploadToS3 = function (req, res, next) {};
+updatePGid = function (req, res, next) {};
