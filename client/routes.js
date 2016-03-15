e083be82756a39ec4b14ace3b2190f8c82d1a8e9
Daniel Wong <danielwong2268@gmail.com>
2016-03-14 19:49:34 -0700

    Add a basic route file in components

---
+++ client/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route} from 'react-router';
+
+// import component modules as needed
+
+export default function getRoutes() {
+  return (
+    <Route path='/' component={App}>
+    </Route>
+  )
+}
