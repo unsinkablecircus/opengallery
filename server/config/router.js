0ad85d40fd42819463fa81bf1e2ab5a182bbff17
Megan Ring <meganbethring@gmail.com>
2016-03-15 15:09:35 -0700

    Update user controller import

--- server/config/router.js
+++ server/config/router.js
@@ -1,8 +1,13 @@
 var helpers = require('./helpers');
 
 var User = require('../controllers/users');
+var Media = require('../controllers/media.js');
 
 module.exports = function (app, express) {
   app.post('/api/user/signIn', User.signIn);
   app.post('/api/user/signUp', User.signUp);
+
+  app.get('/api/media/', Media.getPhotos);
+  app.post('/api/media/upload', Media.uploadPhoto);
 };
+
