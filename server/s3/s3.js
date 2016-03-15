f74b6dbf4d1a7612bb2cb8096b631a1a6d125b71
Megan Ring <meganbethring@gmail.com>
2016-03-15 13:46:35 -0700

    Init s3 module

---
+++ server/s3/s3.js
@@ -0,0 +1,30 @@
+/* Example of connecting to 'opengallery' S3 bucket and executing methods
+More details here: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
+*/
+var s3 = new AWS.S3();
+s3.listBuckets(function(err, data) {
+  if (err) { console.log("Error:", err); }
+  else {
+    for (var index in data.Buckets) {
+      var bucket = data.Buckets[index];
+      console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+    }
+  }
+});
+/*
+var params = {
+  Bucket: 'opengallery', // required 
+  Key: 'TEST_KEY', // required
+  ACL: 'public-read',
+  Body: 'TEST_BODY'
+};
+*/
+s3.putObject(params, function(err, data) {
+  if (err) {
+    console.log("Error uploading data: ", err);
+  } else {
+    console.log("Successfully uploaded data to myBucket/myKey: ", data);
+  }
+});
+
+module.exports = s3;
