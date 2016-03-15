a721f6cd7e788ca346556f5e40cea230a4347b80
John Domingo <johnwill.domingo@gmail.com>
2016-03-15 11:41:37 -0700

    Refactor to functional presentational component

--- client/components/grid/Grid.js
+++ client/components/grid/Grid.js
@@ -3,34 +3,34 @@ import GridTile from './GridTile'
 
 import GridList from 'material-ui/lib/grid-list/grid-list'
 
-export default class Grid extends React.Component {
-  render () {
-    const styles = {
-      grid: {
-        position: 'absolute',
-        width: '100vw',
-        height: '100vh',
-        overflowY: 'auto',
-        paddingTop: 68
-      }
-    }
-
-    const images = []
-    for (var i = 285; i >= 1; i--) {
-      images.push(`http://gratisography.com/pictures/${i}_1.jpg`)
-    }
-
-    return (
-      <GridList
-        cellHeight={300}
-        padding={15}
-        cols={3}
-        style={styles.grid}
-      >
-        {images.map(image => (
-          <GridTile img={image}/>
-        ))}
-      </GridList>
-    )
+const styles = {
+  grid: {
+    position: 'absolute',
+    width: '100vw',
+    height: '100vh',
+    overflowY: 'auto',
+    paddingTop: 68
   }
 }
+
+const images = []
+for (var i = 285; i >= 1; i--) {
+  images.push(`http://gratisography.com/pictures/${i}_1.jpg`)
+}
+
+const Grid = ({media}) => {
+  return (
+    <GridList
+      cellHeight={300}
+      padding={15}
+      cols={3}
+      style={styles.grid}
+    >
+      {images.map(image => (
+        <GridTile img={image}/>
+      ))}
+    </GridList>
+  )
+}
+
+export default Grid
