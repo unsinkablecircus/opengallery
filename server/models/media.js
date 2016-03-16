1e5cb7eea44f0ec9fec51dc3cf88342bf4423437
Megan Ring <meganbethring@gmail.com>
2016-03-15 20:29:16 -0700

    Server writes to PG DB

--- server/models/media.js
+++ server/models/media.js
@@ -1,9 +1,8 @@
-const pg import ('../db/database')
-const s3 import ('../s3/s3')
-const bodyParser import ('body-parser')
-const Promise import ('bluebird')
+const Promise = require('bluebird')
+const PG = Promise.promisifyAll(require('./../db/database'))
+const bodyParser = require('body-parser')
 
-const Media import ('../models/media')
+const Media = require('../models/media')
 
 //controller handles routes
 
@@ -21,57 +20,47 @@ uploadToPG = function (req, res, next) {
   var results = [];
   // Grab data from http request
   var data = {
-    user: req.body.user,
-    url_small: req.body.url_small,
-    url_med: req.body.url_med,
-    url_large: req.body.url_large,
-    title: req.body.title,
-    description: req.body.description
+    // user: req.body.user,
+    // url_small: req.body.url_small,
+    // url_med: req.body.url_med,
+    // url_large: req.body.url_large,
+    // title: req.body.title,
+    // description: req.body.description
+    user: 3,
+    url_small: 'small.url',
+    url_med: 'med.url',
+    url_large: 'large.url',
+    title: 'myMedia',
+    description: 'made with love'
   };
-
-  // Get a Postgres client from the connection pool
-  pg.connect(function(err, client, done) {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log('failed to connect:', err);
-      return res.status(500).json({ success: false, data: err});
-    }
-    // SQL Query > Insert Data
-    client.query(
-      `INSERT INTO media(user, url_small, url_med, url_large, title, description) 
-      values({$1}, {$2}, {$3}, {$4}, {$5}, {$6}) 
-      RETURNING id`, 
-      [
-        data.user, 
-        data.url_small, 
-        data.url_med, 
-        data.url_large, 
-        data.title, 
-        data.description
-      ]
-    ).then(function(data) {
-      console.log(data);
-      // client.raw()
-      
-    });
-    // SQL Query > Select Data
-    // var query = client.query(
-    //   "SELECT id FROM media "
-    // );
-
-    // // Stream results back one row at a time
-    // query.on('row', function(row) {
-    //   results.push(row);
-    // });
-
-    // After all data is returned, close connection and return results
-    query.on('end', function() {
-      done();
-      return res.json(results);
-    });
+  // SQL Query > Insert Data
+  PG.raw(
+    `INSERT INTO media (user_id, url_small, url_medium, url_large, title, description) 
+    values(
+      ${data.user}, 
+      '${data.url_small}', 
+      '${data.url_med}', 
+      '${data.url_large}', 
+      '${data.title}', 
+      '${data.description}') 
+    RETURNING id`
+  ).then(function(data) {
+    console.log(data);
+    // client.raw()
+  })
+  .catch(function(err) {
+    console.log(err);
   });
+
+  // After all data is returned, close connection and return results
+  // query.on('end', function() {
+  //   done();
+  //   return res.json(results);
+  // });
 };
+
+//test function
+uploadToPG();
 
 uploadToS3 = function (req, res, next) {
   
