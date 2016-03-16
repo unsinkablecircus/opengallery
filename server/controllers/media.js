bf11b7cd85844ec8bb6a47746957aa23c653a92e
Megan Ring <meganbethring@gmail.com>
2016-03-16 15:46:33 -0700

    Refactor mediaController to handle photoParsing, calling mediaModel functions

--- server/controllers/media.js
+++ server/controllers/media.js
@@ -2,41 +2,25 @@ const pg = require('../db/database')
 const s3 = require('../s3/s3')
 const bodyParser = require('body-parser')
 const Promise = require('bluebird')
+const jimp = require('jimp')
 
 const Media = require('../models/media')
 
 
 
 //controller handles routes
+//invoke functions which write to PG
 
 exports.uploadPhoto = function (req, res) {
   //parse data to separate photodata from photo
   var photoAll = req.body.photo;
-  uploadToPG(photoData, function(){
-    uploadToS3(photo, function(){
-      updatePGid(photoId)
+  var photoInfo = req.body.photoInfo;
+  // var photoRaw = req.body.photoRaw;
+  //
+  Media.uploadToPG(photoInfo, function(id){
+    var url = //unsinkkableCircus url + id + size
+    Media.uploadToS3(photo, function(url){
+      Media.updatePGid(photoId);
     })
   })
 };
-
-//promisified w/knex
-uploadToPG = function (req, res, next) {};
-
-//need to promisify
-uploadToS3 = function (photo, url, next) {
-  
-  /*
-  var params = {
-    Bucket: 'opengallery', // required 
-    Key: 'TEST_KEY', // required
-    ACL: 'public-read',
-    Body: 'TEST_BODY' //new Buffer('...') || 'STRING_VALUE' || streamObject,
-  };
-  */
-  s3.putObject(params, function(url) {
-
-  })
-};
-
-//promisified w/knex
-updatePGid = function (req, res, next) {};
