9a3d5b28a3be77ea24c892a9bc0181fec485b618
John Domingo <johnwill.domingo@gmail.com>
2016-03-16 01:52:34 -0700

    Refactor to be component-centric; add Grid spec

--- test/client/client.spec.js
+++ test/client/client.spec.js
@@ -1,8 +1,10 @@
+import { GridComponent } from './components/grid'
 import { GridActions } from './actions/grid'
 import { GridReducers } from './reducers/grid'
 
 describe('CLIENT TESTS', () => {
   describe('Grid', () => {
+    describe('Component', GridComponent)
     describe('Actions', GridActions)
     describe('Reducers', GridReducers)
   })
