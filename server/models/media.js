8b3dc20186fd4e16fd9bf503bf8441bda64218b6
Megan Ring <meganbethring@gmail.com>
2016-03-15 17:27:08 -0700

    Write rough db query for posting media data

--- server/models/media.js
+++ server/models/media.js
@@ -1,45 +1,126 @@
-// upload media to s3 (async)
-// on upload, store image url to postgreSQL db
-const pg = require('../db/database')
+const pg import ('../db/database')
+const s3 import ('../s3/s3')
+const bodyParser import ('body-parser')
+const Promise import ('bluebird')
 
-const Media = pg.Model.extend({
-  tableName: 'media',
-  hasTimestamps: true
-})
+const Media import ('../models/media')
 
+//controller handles routes
 
-
-module.exports = Media;
+exports.uploadPhoto = function (req, res) {
+  //parse data to separate photodata from photo
+  var photoAll = req.body.photo;
+  uploadToPG(photoData, function(){
+    uploadToS3(photo, function(){
+      updatePGid(photoId)
+    })
+  })
+};
 
 /*
-// from example collections file
-var db = require('../config');
-var Link = require('../models/link');
+router.post('/api/v1/todos', function(req, res) {
 
-var Links = new db.Collection();
+  var results = [];
 
-Links.model = Link;
+  // Grab data from http request
+  var data = {text: req.body.text, complete: false};
 
-module.exports = Links;
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err});
+    }
 
-// from example model file
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  clicks: function() {
-    return this.hasMany(Click);
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
+    // SQL Query > Insert Data
+    client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+        results.push(row);
     });
-  }
-});
 
-module.exports = Link;
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+});
 */
+
+uploadToPG = function (req, res, next) {
+  var results = [];
+  // Grab data from http request
+  var data = {
+    user: req.body.user,
+    url_small: req.body.url_small,
+    url_med: req.body.url_med,
+    url_large: req.body.url_large,
+    title: req.body.title,
+    description: req.body.description
+  };
+
+  // Get a Postgres client from the connection pool
+  pg.connect(function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log('failed to connect:', err);
+      return res.status(500).json({ success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    client.query(
+      "INSERT INTO media(user, url_small, url_med, url_large, title, description) values($1, $2, $3, $4, $5, $6)", 
+      [
+        data.user, 
+        data.url_small, 
+        data.url_med, 
+        data.url_large, 
+        data.title, 
+        data.description
+      ]
+    ).then(function(data) {
+      console.log(data);
+      // client.raw()
+      
+    });
+    // SQL Query > Select Data
+    // var query = client.query(
+    //   "SELECT id FROM media "
+    // );
+
+    // // Stream results back one row at a time
+    // query.on('row', function(row) {
+    //   results.push(row);
+    // });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+};
+
+uploadToS3 = function (req, res, next) {
+  
+  /*
+  var params = {
+    Bucket: 'opengallery', // required 
+    Key: 'TEST_KEY', // required
+    ACL: 'public-read',
+    Body: 'TEST_BODY'
+  };
+  */
+  s3.putObject(params, function(url) {
+
+  })
+};
+
+updatePGid = function (req, res, next) {};
