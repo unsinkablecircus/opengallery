6d7b7b660104b3186b81b0506f922aad6eb24c72
Megan Ring <meganbethring@gmail.com>
2016-03-15 17:23:25 -0700

    add note about what controller file does

--- server/controllers/media.js
+++ server/controllers/media.js
@@ -1,12 +1,41 @@
 const pg import ('../db/database')
 const s3 import ('../s3/s3')
+const bodyParser import ('body-parser')
+const Promise import ('bluebird')
+
+const Media import ('../models/media')
+
+
+
+//controller handles routes
 
 exports.uploadPhoto = function (req, res) {
-  uploadToPG()
-  uploadToS3()
-  updatePGid()
+  //parse data to separate photodata from photo
+  var photoAll = req.body.photo;
+  uploadToPG(photoData, function(){
+    uploadToS3(photo, function(){
+      updatePGid(photoId)
+    })
+  })
 };
 
 uploadToPG = function (req, res, next) {};
-uploadToS3 = function (req, res, next) {};
+
+//need to promisify
+uploadToS3 = function (req, res, next) {
+  
+  /*
+  var params = {
+    Bucket: 'opengallery', // required 
+    Key: 'TEST_KEY', // required
+    ACL: 'public-read',
+    Body: 'TEST_BODY'
+  };
+  */
+  s3.putObject(params, function(url) {
+
+  })
+};
+
+//use knex to 
 updatePGid = function (req, res, next) {};
