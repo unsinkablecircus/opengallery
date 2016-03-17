ab3b2ada01c55ccb312b6cc1afee649493265b20
John Domingo <johnwill.domingo@gmail.com>
2016-03-16 22:33:39 -0700

    Add GRID_FILTER action and use mock default state
    Refactor grid actions to focus only on grid state

--- test/client/actions/grid.js
+++ test/client/actions/grid.js
@@ -1,65 +1,41 @@
 import { expect } from 'chai'
 import * as actions from '../../../client/actions/grid'
+import { initialState as state } from '../../initialState'
 
 export const GridActions = () => {
-  it('should create a GRID_REQUEST action to fetch grid data', (done) => {
-    const tags = ['pink', 'cats']
-    expect(actions.requestGridData(tags)).to.deep.equal({
+  it('should create a GRID_FILTER action to set filter tags', () => {
+    expect(actions.filterGridData(state.grid.filter)).to.deep.equal({
+      type: actions.GRID_FILTER,
+      payload: state.grid.filter
+    })
+  })
+
+  it('should create a GRID_REQUEST action to fetch grid data', () => {
+    expect(actions.requestGridData()).to.deep.equal({
       type: actions.GRID_REQUEST,
-      payload: tags,
-      meta: {
-        fetching: true,
-        success: false
-      }
+      payload: true
     })
-    done()
   })
 
-  it('should create a GRID_SUCCESS action to receive grid data', (done) => {
-    const data = {
-      grid: ['1'],
-      media: {
-        '1': {
-          userId: '1',
-          userFeedbackId: '1',
-          media: 'Photograph',
-          title: 'Sample Image',
-          description: 'This is a test image.',
-          tags: ['pink', 'cats'],
-          url_sm: '/assets/image-sm',
-          url_md: '/assets/image-md',
-          url_lg: '/assets/image-lg',
-          feedback: ['1']
-        }
-      },
-      feedback: {
-        '1': {
-          hashtag: 'cool',
-          count: '3'
-        }
-      }
-    }
-    expect(actions.receiveGridData(data)).to.deep.equal({
+  it('should create a GRID_SUCCESS action to receive grid data', () => {
+    expect(actions.receiveGridData(state.grid.tiles)).to.deep.equal({
       type: actions.GRID_SUCCESS,
-      payload: {...data},
+      payload: state.grid.tiles,
+      error: '',
       meta: {
-        fetching: false,
-        success: true
+        fetching: false
       }
     })
-    done()
   })
 
-  it('should create a GRID_FAILURE action to catch errors', (done) => {
+  it('should create a GRID_FAILURE action to catch errors', () => {
     const error = new Error('Grid Failure')
     expect(actions.catchGridData(error)).to.deep.equal({
       type: actions.GRID_FAILURE,
       error,
       meta: {
-        fetching: false,
-        success: false
+        fetching: false
       }
     })
-    done()
   })
 }
