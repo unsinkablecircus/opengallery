747744363beb00c0b9cc243184e224ccf36e4c22
John Domingo <johnwill.domingo@gmail.com>
2016-03-16 23:21:25 -0700

    Simplify tests using mock intial state data
    Implement additional test for new GRID_FILTER action

--- test/client/reducers/grid.js
+++ test/client/reducers/grid.js
@@ -1,68 +1,43 @@
 import { expect } from 'chai'
-import * as actions from '../../../client/actions/grid'
 import reducer from '../../../client/reducers/grid'
+import * as actions from '../../../client/actions/grid'
+import { initialState as state } from '../../initialState'
 
 export const GridReducers = () => {
-  it('should return the initial state', (done) => {
-    expect(reducer(undefined, {})).to.deep.equal({
-      isFetchingMedia: false,
-      errorFetchingMedia: '',
-      grid: [],
-      media: {},
-      feedback: {}
-    })
-    done()
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).to.deep.equal(state.grid)
   })
 
-  it('should handle a GRID_REQUEST created from requestGridData', (done) => {
+  it('should handle a GRID_FILTER action created from filterGridData', () => {
     const tags = ['pink', 'cats']
-    const action = actions.requestGridData(tags)
+    const action = actions.filterGridData(tags)
     expect(reducer({}, action)).to.deep.equal({
-      isFetchingMedia: true
+      filter: tags
     })
-    done()
   })
 
-  it('should handle a GRID_SUCCESS created from receiveGridData', (done) => {
-    const data = {
-      grid: ['1'],
-      media: {
-        '1': {
-          userId: '1',
-          userFeedbackId: '1',
-          media: 'Photograph',
-          title: 'Sample Image',
-          description: 'This is a test image.',
-          tags: ['pink', 'cats'],
-          url_sm: '/assets/image-sm',
-          url_md: '/assets/image-md',
-          url_lg: '/assets/image-lg',
-          feedback: ['1']
-        }
-      },
-      feedback: {
-        '1': {
-          hashtag: 'cool',
-          count: '3'
-        }
-      }
-    }
-    const action = actions.receiveGridData(data)
+  it('should handle a GRID_REQUEST created from requestGridData', () => {
+    const action = actions.requestGridData()
     expect(reducer({}, action)).to.deep.equal({
-      isFetchingMedia: false,
-      errorFetchingMedia: '',
-      ...data
+      fetching: true
     })
-    done()
   })
 
-  it('should handle a GRID_FAILURE created from catchGridData', (done) => {
+  it('should handle a GRID_SUCCESS created from receiveGridData', () => {
+    const action = actions.receiveGridData(state.grid.tiles)
+    expect(reducer({}, action)).to.deep.equal({
+      fetching: false,
+      error: '',
+      tiles: state.grid.tiles
+    })
+  })
+
+  it('should handle a GRID_FAILURE created from catchGridData', () => {
     const error = new Error('Grid Failure')
     const action = actions.catchGridData(error)
     expect(reducer({}, action)).to.deep.equal({
-      isFetchingMedia: false,
-      errorFetchingMedia: error
+      fetching: false,
+      error: error
     })
-    done()
   })
 }
