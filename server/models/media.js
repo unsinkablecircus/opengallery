25d246e5153f514b828f6052ad0fa67cd72259f6
Megan Ring <meganbethring@gmail.com>
2016-03-17 13:52:25 -0700

    Move conflicting s3 and PG example info media model

--- server/models/media.js
+++ server/models/media.js
@@ -74,6 +74,40 @@ exports.updatePGid = function (photosURLsArr) {
   )
 };
 
+// example of connecting to postgresql database below (will move to models later):
+// var client = new pg.Client(connectionInfo);
+// client.connect();
+// var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+// query.on('end', function() { client.end(); });
+
+/* Example of connecting to 'opengallery' S3 bucket and executing methods
+More details here: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
+var s3 = new AWS.S3();
+s3.listBuckets(function(err, data) {
+  if (err) { console.log("Error:", err); }
+  else {
+    for (var index in data.Buckets) {
+      var bucket = data.Buckets[index];
+      console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+    }
+  }
+});
+
+var params = {
+  Bucket: 'opengallery', // required 
+  Key: 'TEST_KEY', // required
+  ACL: 'public-read',
+  Body: 'TEST_BODY'
+};
+s3.putObject(params, function(err, data) {
+  if (err) {
+    console.log("Error uploading data: ", err);
+  } else {
+    console.log("Successfully uploaded data to myBucket/myKey: ", data);
+  }
+});
+*/
+
 //example s3 function
 let listBuckets = function() {
   s3.listBuckets(function(err, data) {
