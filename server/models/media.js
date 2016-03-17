f70cbf1ed19a139ad680d54ab3fff818aa8e476d
Megan Ring <meganbethring@gmail.com>
2016-03-16 17:03:02 -0700

    Refactor updatedPGid function to accept proper parameter

--- server/models/media.js
+++ server/models/media.js
@@ -2,33 +2,18 @@ const Promise = require('bluebird')
 const pg = require('./../db/database')
 const s3 = require('./../s3/s3')
 const bodyParser = require('body-parser')
-const Jimp = require("jimp")
 
 const Media = require('../models/media')
 
 //model handles db manipulation
 
-exports.uploadPhoto = function (req, res) {
-  //parse data to separate photodata from photo
-  var photoAll = req.body.photo;
-  var photoInfo = req.body.photoInfo;
-  var photoRaw = req.body.photoRaw;
-  uploadToPG(photoData, function(id){
-    var url = 
-    uploadToS3(photo, function(url){
-      updatePGid(photoId);
-    })
-  })
-};
-
-uploadToPG = function (photoData, res, next) {
-  // Grab data from http request
+exports.uploadToPG = function (photoData, cb) {
   /* example data
   var data = {
     user: 3,
-    url_small: 'small.url',
-    url_med: 'med.url',
-    url_large: 'large.url',
+    url_small: 'null',
+    url_med: 'null',
+    url_large: 'null',
     title: 'myPicture',
     description: 'made with love'
   };
@@ -45,55 +30,35 @@ uploadToPG = function (photoData, res, next) {
       '${photoData.description}') 
     RETURNING id`
   ).then(function(data) {
-    next(data)
+    cb(null, data)
   })
   .catch(function(err) {
-    next(err);
+    cb(err, null);
   });
 };
 
 // test function
 // uploadToPG();
 
-uploadToS3 = function (photosArr, id) {
-  /*
+exports.uploadToS3 = function (photoId, photo) {
   var params = {
     Bucket: 'opengallery', // required 
-    Key: 'TEST_KEY', // required
+    Key: photoId, // required
     ACL: 'public-read',
-    Body: 'TEST_BODY'
+    Body: photo
   };
-  */
-  var photoIdsArr = [];
-
-  new Promise = function(resolve, reject) {
-    s3.putObject(params, function small(err, url) {
-
-    }).
-    catch(function(err) {
-
-    })
-    .
-  }
-  s3.putObject(params, function med(err, url) {
-
-  })
-  s3.putObject(params, function large(err, url) {
-
-  })
+  s3.putObject(params)
 };
 
-updatePGid = function (photoIdsArr) {
+exports.updatePGid = function (photoURLsArr) {
   //array order is small, med, large
   pg.raw(
     `UPDATE media (url_small, url_medium, url_large)
     values(
-      '${photosIdArr[0]}',
-      '${photosIdArr[1]}',
-      '${photosIdArr[2]}',
+      '${photosURLsArr[0]}',
+      '${photosURLsArr[1]}',
+      '${photosURLsArr[2]}',
     )
     `
   )
-  .then()
-  .catch();
 };
