beb6ac2f568cfcd184df650dd667034431513846
Evan Chen <evanwchen@gmail.com>
2016-03-18 11:57:24 -0700

    Add flexbox layout with Input Form for user profile settings

--- client/components/user/User.js
+++ client/components/user/User.js
@@ -9,13 +9,13 @@ import Avatar from 'material-ui/lib/avatar';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 import TextField from 'material-ui/lib/text-field';
+import FlatButton from 'material-ui/lib/flat-button';
 import Settings from 'material-ui/lib/svg-icons/action/settings';
 import Email from 'material-ui/lib/svg-icons/communication/email';
 import Phone from 'material-ui/lib/svg-icons/communication/phone';
 import Edit from 'material-ui/lib/svg-icons/editor/mode-edit';
 import AddPhoto from 'material-ui/lib/svg-icons/image/add-a-photo';
-
-
+import Person from 'material-ui/lib/svg-icons/social/person';
 import Colors from 'material-ui/lib/styles/colors';
 
 const User = () => {
@@ -29,26 +29,48 @@ const User = () => {
             src="http://i.giphy.com/6RwY3KrjXVY7m.gif"
             className="user-avatar"
           />
-          <br/>
-          <List>
-            <ListItem>
-              Username
-            </ListItem>
-          </List>
-          
-          <Settings color={Colors.red500} /> Settings <br/>
-          <Email color={Colors.red500} /> Email <br/>
-          <Phone color={Colors.red500} /> Phone <br/>
-          <Edit color={Colors.red500} /> Edit <br/>
-          <AddPhoto color={Colors.red500} /> Upload an Image <br/>
-
-          <TextField
-            hintText="Hint Text"
-          /><br/>
-          <br/>
-          <TextField
-            hintText="Hint Text"
+          <FlatButton
+            label="Edit Profile"
+            linkButton={true}
+            href="http://localhost:8000/profile/evanchen"
+            secondary={true}
+            icon={<Edit color={Colors.red500} className="user-icon" />}
           />
+
+          <div className="user-details">
+            <div className="user-row">
+              <Person color={Colors.red500} className="user-icon" />
+              <TextField
+                disabled={true}
+                defaultValue="Disabled Value"
+                className="user-field"
+              />
+            </div>
+            <div className="user-row">
+              <Email color={Colors.red500} className="user-icon" />
+              <TextField
+                disabled={true}
+                defaultValue="Disabled Value"
+                className="user-field"
+              />
+            </div>
+            <div className="user-row">
+              <Phone color={Colors.red500} className="user-icon" />
+              <TextField
+                disabled={true}
+                defaultValue="Disabled Value"
+                className="user-field"
+              />
+            </div>
+            <div className="user-row">
+              <AddPhoto color={Colors.red500} className="user-icon" />
+              <TextField
+                disabled={true}
+                defaultValue="Disabled Value"
+                className="user-field"
+              />
+            </div>
+          </div>
         </section>
         <section className="user-right">
           <Grid/>
