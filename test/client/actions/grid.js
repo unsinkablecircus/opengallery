f0bdca4705d3c73cd5fbabbe2a4a82ff4806ddc2
John Domingo <johnwill.domingo@gmail.com>
2016-03-19 11:27:58 -0700

    Simplify expectations of grid action tests

--- test/client/actions/grid.js
+++ test/client/actions/grid.js
@@ -3,21 +3,21 @@ import * as grid from '../../../client/actions/grid'
 import { initialState as state } from '../../initialState'
 
 export const GridActions = () => {
-  it('should create a GRID_FILTER action to set filter tags', () => {
+  it('should create a GRID_FILTER action', () => {
     expect(grid.filterGridData(state.media.filter)).to.deep.equal({
       type: grid.GRID_FILTER,
       payload: state.media.filter
     })
   })
 
-  it('should create a GRID_REQUEST action to fetch grid data', () => {
+  it('should create a GRID_REQUEST action', () => {
     expect(grid.requestGridData()).to.deep.equal({
       type: grid.GRID_REQUEST,
       payload: true
     })
   })
 
-  it('should create a GRID_SUCCESS action to receive grid data', () => {
+  it('should create a GRID_SUCCESS action', () => {
     expect(grid.receiveGridData(state.media.grid)).to.deep.equal({
       type: grid.GRID_SUCCESS,
       payload: state.media.grid,
@@ -28,7 +28,7 @@ export const GridActions = () => {
     })
   })
 
-  it('should create a GRID_FAILURE action to catch errors', () => {
+  it('should create a GRID_FAILURE action', () => {
     const error = new Error('Grid Failure')
     expect(grid.catchGridData(error)).to.deep.equal({
       type: grid.GRID_FAILURE,
