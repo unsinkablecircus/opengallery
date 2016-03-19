e4c9f858fc0a29359e8c862e8a6bf5801089153f
John Domingo <johnwill.domingo@gmail.com>
2016-03-19 12:23:48 -0700

    Shorten action creator names
    Action creators remain semantic upon invocation provided that the actions are imported as the related component's name. For example, gallery actions are imported as 'gallery' and action creators then become gallery.nextTile().

--- client/actions/gallery.js
+++ client/actions/gallery.js
@@ -2,19 +2,19 @@ export const SHOW_NEXT = 'SHOW_NEXT'
 export const SHOW_PREV = 'SHOW_PREV'
 export const TOGGLE_GALLERY = 'TOGGLE_GALLERY'
 
-export function showNextTile() {
+export function nextTile() {
   return {
     type: SHOW_NEXT
   }
 }
 
-export function showPrevTile() {
+export function prevTile() {
   return {
     type: SHOW_PREV
   }
 }
 
-export function toggleGallery(tile) {
+export function toggleView(tile) {
   return {
     type: TOGGLE_GALLERY,
     payload: tile
