1f44792e929ce2fdb84417981da32ff9a59ba615
John Domingo <johnwill.domingo@gmail.com>
2016-03-20 12:15:30 -0700

    Add colors and shadows; organize vars with labels

--- client/components/gallery/GalleryTile.js
+++ client/components/gallery/GalleryTile.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const GalleryTile = ({ img, style }) => (
-  <div>
-    <img className="gallery-photo" src={img} style={style}/>
-  </div>
-)
+const GalleryTile = ({ data, style }) => {
+  style.backgroundImage = `url(${data.url_lg})`
+  return (
+    <div className="gallery-photo" style={style}>
+      <div className="overlay"></div>
+      <div className="corner-overlay-content">Info</div>
+      <div className="overlay-content">
+        <h2>{data.title}</h2>
+        <p>{data.description}</p>
+      </div>
+    </div>
+  )
+}
 
 export default GalleryTile
