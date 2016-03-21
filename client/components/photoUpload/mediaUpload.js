86f0016f00564ef40e1273a4630dd9b03c32287a
Megan Ring <meganbethring@gmail.com>
2016-03-21 12:27:10 -0700

    Create directory and file for new upload model; psuedocode data flow for new component

---
+++ client/components/photoUpload/mediaUpload.js
@@ -0,0 +1,5 @@
+//on click => dispatch action: show photo upload
+//action: show photo upload => update state on model component (this?)
+// on drop (use drop zone) => dispatch 'isUploading' function (similar to authActions file)
+// on http request for photoUpload endpoint, dispatch 'uploaded' action
+// 
