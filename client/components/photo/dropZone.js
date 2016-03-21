1ca9db99e235ba24d998d2044ddcddf4d34713b0
Megan Ring <meganbethring@gmail.com>
2016-03-21 16:18:15 -0700

    Define dropZone component using example as skeleton

--- client/components/photo/dropZone.js
+++ client/components/photo/dropZone.js
@@ -1,1 +1,25 @@
+import React from 'react';
 import dropZone from 'react-dropzone'
+
+const DropZone = ({
+  /*states*/
+}) => {
+  const actions = [];
+
+  return (
+    <div>
+      <Dropzone ref="dropzone" onDrop={this.onDrop} >
+        <div>Try dropping some files here, or click to select files to upload.</div>
+      </Dropzone>
+      <button type="button" onClick={this.onOpenClick}>
+          Open Dropzone
+      </button>
+      {this.state.files ? <div>
+      <h2>Uploading {files.length} files...</h2>
+      <div>this.state.files.map((file) => <img src={file.preview} />)</div>
+      </div> : null}
+    </div>
+  );
+}
+
+export default DropZone;
