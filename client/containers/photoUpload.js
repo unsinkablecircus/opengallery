de72dfa1d2a7f79a0004748cef8c2f2f9abe0911
Megan Ring <meganbethring@gmail.com>
2016-03-21 15:43:45 -0700

    Define photoUpload container, model basic structure for photoUpload container

--- client/containers/photoUpload.js
+++ client/containers/photoUpload.js
@@ -1,1 +1,81 @@
+import { connect } from 'react-redux'
+import PhotoUpload from '../components/photo/PhotoUpload'
+//import other actions from actions folder
 
+const mapStateToProps = (state) => {
+  //update props with relevent states
+  return {
+    isUploaded: state.photo.isUploaded,
+    showPhotoInGalley: state.photo.showPhotoInGalley,
+    error: state.photo.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onUploadSubmit: (photo, metaData) => {
+      dispatch(/**/)
+    },
+    onUploadClick: () => {
+      dispatch({
+        type: 'TOGGLE_UPLOADPHOTO_MODAL'
+      })
+    }
+  }
+}
+
+const container = connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(PhotoUpload);
+
+export default container;
+
+/*
+import { connect } from 'react-redux'
+import Auth from '../components/auth/Auth' 
+import { SignupUser } from '../actions/signup' 
+import { SigninUser } from '../actions/signin'
+
+const mapStateToProps = (state) => {
+  // return object with the relevant state. related to DATA
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    showSigninAndNotSignup: state.auth.showSigninAndNotSignup,
+    error: state.auth.error
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  // return object with the props that depend on a dispatch action. related to BEHAVIOR
+  return {
+    onSigninModalClick: () => {
+      // refactor to extract these actions
+      dispatch({
+        type: 'TOGGLE_SIGNIN_MODAL'
+      })
+    },
+    // this will be called when you hear back from an ajax request.
+    onSigninSubmit: (creds) => {
+      dispatch(SigninUser(creds));
+    },
+    onSignupSubmit: (creds) => {
+      dispatch(SignupUser(creds));
+    },
+    toggleSigninOrSignupLink: () => {
+      dispatch({
+        type: 'TOGGLE_SIGNIN_OR_SIGNUP_LINK'
+      })
+    }
+  }
+}
+
+// tentative convention will be that containers are lower case, while their corresponding presentational components are upper case
+const container = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Auth);
+
+export default container;
+*/
