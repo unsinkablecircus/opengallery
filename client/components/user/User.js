a04b734d8250bde0b40c153c9891d4576d392f03
Evan Chen <evanwchen@gmail.com>
2016-03-21 12:11:30 -0700

    Remove React-Redux 'connect' module from Auth and User presentational components.

--- client/components/user/User.js
+++ client/components/user/User.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Grid from '../../containers/grid';
 
