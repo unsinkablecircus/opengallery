c2205794fee6a0cf7fd253a9abc356bf5a8601ac
Megan Ring <meganbethring@gmail.com>
2016-03-22 11:16:08 -0700

    Add mapDipatchToProps methood which handles upload modal

--- client/containers/app.js
+++ client/containers/app.js
@@ -1,7 +1,18 @@
 import { connect } from 'react-redux'
 import App from '../components/App'
 
+
+const mapDispatchToProps = (dispatch) => {
+  onToggleUpload: () => {
+    dispatch({
+      type: 'TOGGLE_PHOTOUPLOAD_MODAL'
+    });
+  }
+}
+
 const mapStateToProps = (state, ownProps) => {
+
+
   return {
     displayGallery: state.view.displayGallery,
     isUploadModalOpen: state.view.isUploadModalOpen
