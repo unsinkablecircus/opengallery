961b187d0e4d831014487b834c8f73e73f53a4c2
John Domingo <johnwill.domingo@gmail.com>
2016-03-23 15:03:27 -0700

    Separate toggle into show and hide actions

--- client/reducers/view.js
+++ client/reducers/view.js
@@ -1,6 +1,6 @@
 import { initialState } from '../../test/initialState'
 import { TOGGLE_GALLERY } from '../actions/gallery'
-import { TOGGLE_WORDMAP } from '../actions/wordmap.actions'
+import { SHOW_WORDMAP, HIDE_WORDMAP } from '../actions/wordmap.actions'
 
 const view = (state = initialState.view, action) => {
   switch (action.type) {
@@ -8,9 +8,13 @@ const view = (state = initialState.view, action) => {
       return Object.assign({}, state, {
         displayGallery: !state.displayGallery
       })
-    case TOGGLE_WORDMAP:
+    case SHOW_WORDMAP:
       return Object.assign({}, state, {
-        displayWordmap: !state.displayWordmap
+        displayWordmap: true
+      })
+    case HIDE_WORDMAP:
+      return Object.assign({}, state, {
+        displayWordmap: false
       })
     default:
       return state
