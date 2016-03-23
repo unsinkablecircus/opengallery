e5cd294a20f63b14afd7376269b6b3f83a87e2ae
Megan Ring <meganbethring@gmail.com>
2016-03-23 15:42:16 -0700

    Add cancel button

--- client/components/photo/dropZone.js
+++ client/components/photo/dropZone.js
@@ -4,27 +4,26 @@ import FlatButton from 'material-ui/lib/flat-button';
 
 const DropZone = ({
   userId, 
+  onUploadCancel,
   onPhotoDrop,
   isDropOpen,
   currentFileUploading
 }) => {
   const actions = [
     
-    
   ];
 
-  console.log('inside dropzone and the currentFileUploading is: ', currentFileUploading);
+  console.log('inside dropzone and the currentFileUploading is: ', currentFileUploading[0]);
   return (
-    <div actions={ actions }>
+    <div>
     {
       //how to pass in current user?
     }
       <Dropzone ref="dropzone" onDrop={ onPhotoDrop } >
         <div>Click inside or drop file to upload.</div>
       </Dropzone>
-      <FlatButton label="Open" primary={true} type="button" onClick={ isDropOpen }>
-        Open
-      </FlatButton>
+      <FlatButton label="Open" primary={true} type="button" onClick={ isDropOpen } />
+      <FlatButton label="Cancel" primary={true} type="button" onClick={ onUploadCancel } />
       {currentFileUploading ? <div>
       <h2>Uploading {currentFileUploading.length} file...</h2>
       <div>
