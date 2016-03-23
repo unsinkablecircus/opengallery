1e7200f7ea41fa1612e3012c2fc5fb7c538469b5
John Domingo <johnwill.domingo@gmail.com>
2016-03-23 11:28:19 -0700

    Catch TOGGLE_WORDMAP action in view reducer

--- client/reducers/view.js
+++ client/reducers/view.js
@@ -1,11 +1,16 @@
 import { initialState } from '../../test/initialState'
 import { TOGGLE_GALLERY } from '../actions/gallery'
+import { TOGGLE_WORDMAP } from '../actions/wordmap.actions'
 
 const view = (state = initialState.view, action) => {
   switch (action.type) {
     case TOGGLE_GALLERY:
       return Object.assign({}, state, {
         displayGallery: !state.displayGallery
+      })
+    case TOGGLE_WORDMAP:
+      return Object.assign({}, state, {
+        displayWordmap: !state.displayWordmap
       })
     default:
       return state
