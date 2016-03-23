beb3c64c0993c269a3ef6ba18d4cabc3e68993d5
John Domingo <johnwill.domingo@gmail.com>
2016-03-23 15:02:09 -0700

    Replace npm bubble chart with customized version

--- client/components/wordmap/Wordmap.js
+++ client/components/wordmap/Wordmap.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import ReactBubbleChart from 'react-bubble-chart'
-
-const colorLegend = [
-  //reds from dark to light
-  {color: "#67000d", text: 'Negative', textColor: "#ffffff"}, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
-  //neutral grey
-  {color: "#f0f0f0", text: 'Neutral'},
-  // blues from light to dark
-  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", {color: "#08306b", text: 'Positive', textColor: "#ffffff"}
-]
+import ReactBubbleChart from './ReactBubbleChart'
 
 const tooltipProps = [{
   css: 'symbol',
@@ -24,10 +15,6 @@ const tooltipProps = [{
 }]
 
 export default class Wordmap extends React.Component {
-  shouldComponentUpdate (nextProps) {
-    return nextProps.displayWordmap;
-  }
-
   render () {
     let { tile, data, dictionary } = this.props
     let sentiment = data.feedback.map((hashtag, i) => ({
@@ -40,17 +27,19 @@ export default class Wordmap extends React.Component {
     return (
       <ReactBubbleChart
         className="wordmap-component"
-        colorLegend={colorLegend}
         data={sentiment}
-        selectedColor="#737373"
-        selectedTextColor="#d9d9d9"
+        selectedColor="white"
+        selectedTextColor="rgba(0, 0, 0, 0.75)"
         fixedDomain={{ min: -1, max: 1 }}
         onClick={{}}
         legend={false}
         legendSpacing={0}
         tooltip={false}
         tooltipProps={tooltipProps}
-        tooltipFunc={{}}
+        tooltipFunc={() => {}}
+        fontSizeFactor={0.5}
+        duration={0}
+        delay={0}
       />
     )
   }
