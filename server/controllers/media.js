2ee11824654ddb27fd9addf56ba1d4a0bba700c0
John Domingo <johnwill.domingo@gmail.com>
2016-03-24 17:39:00 -0700

    Implement photo resize + combine with S3/PG upload

--- server/controllers/media.js
+++ server/controllers/media.js
@@ -1,5 +1,6 @@
 const Promise = require('bluebird')
 const jimp = require('jimp')
+const fs = require('fs')
 
 const Media = require('../models/media')
 
@@ -13,68 +14,96 @@ const separateData = (data) => {
   return photoData
 }
 
-const cloneAndManipulate = (size, image) => {
-  //side is one dimension of photo
-  var side = 0;
-  if (size === 'small') {
-    side = 25;
-  }
-  if (size === 'medium') {
-    side = 800;
-  }
-  photoObj[size] = image
-    .clone()
-    .read(photoObj[size], function(err, image) {
-      if (err) {
-        console.log(`Error reading image`, err);
-      } else {
-        image.resize(Jimp.AUTO, side, Jimp.RESIZE_BEZIER)
-        .getBuffer( jimp.MIME_JPEG, 
-          // convert each photo to buffer
-          function(err, bufferImg) {
-            if (err) {
-              console.log(`Error writing ${size} image: `, err);
-            } else {
-              photoObj[size] = bufferImg;
-            }
-          }
-        );
-      }
-    });
+const resizePhoto = ({ buffer, mimetype }, size, quality) => {
+  return jimp.read(buffer)
+  .then(image => {
+    return new Promise(function(resolve, reject) {
+      image.clone().resize(size, jimp.AUTO).quality(quality)
+      .getBuffer(mimetype, (err, buffer) => {
+        if (err) {
+          console.error(`Error parsing Jimp buffer to ${mimetype}: ${err}`)
+          reject(`Error parsing Jimp buffer to ${mimetype}: ${err}`)
+        } else {
+          resolve(buffer)
+        }
+      })
+    })
+  })
+  .catch(err => {
+    console.error(`Error reading image buffer: ${err}`)
+    throw new Error(`Error reading image buffer: ${err}`)
+  })
 }
 
-const resizePhoto = (photo) => {
-  var photoObj = {
-    small: '',
-    medium: '',
-    large: ''
-  }
-
-  //use jimp to resize original photo
-  Jimp.read(photo, function (err, image) {
-    if (err) {
-      console.log('Error reading image: ', err);
-    } else {
-      // do stuff with the image (if no exception)
-      cloneAndManipulate(small, readImage);
-      cloneAndManipulate(medium, readImage);
-      // cloneAndManipulate(large, readImage);
-    }
-  });
-  return photoObj;
-}
 //controller handles function delegation
 exports.getPhotos = function (req, res) {
   Media.retrievePhotosFromPG()
   .then((photos) => {
+    console.log("Success retrieving photos");
+
     res.status(200).json(photos);
   })
   .catch((err) => {
+    console.log("Error retrieving photos", err);
     res.status(404).send();
   });
 }
 
 exports.uploadPhoto = function (req, res) {
+  var responseObject = {
+    id: null,
+    user_id: 5,
+    url_small: '', //resizedPhotos.small
+    url_med: '',
+    url_large: '',
+    title: '', // photo.PGupload.title
+    description: 'GOT IT' // photo.PGupload.description
+  }
+
+  if (req.file) {
+    resizePhoto(req.file, 25, 0)
+    .then( buffer => {
+      responseObject.url_small = new Buffer(buffer).toString('base64')
+      return resizePhoto(req.file, 800, 0)
+    })
+    .then( mediumBuffer => {
+      Media.uploadToPG(req.body)
+      .then((id) => {
+        // console.log("Id returned from uploadToPG function line 100 of media controllers: ", id.rows[0].id);
+        responseObject.id = id.rows[0].id;
+        var urlExtMedium = responseObject.id + 'medium';
+        var urlExtLarge = responseObject.id + 'large';
+        responseObject.url_med = ('http://d14shq3s3khz77.cloudfront.net/' + urlExtMedium);
+        responseObject.url_large = ('http://d14shq3s3khz77.cloudfront.net/' + urlExtLarge);
+
+        new Promise.all([
+          Media.uploadToS3(urlExtLarge, req.file.buffer), Media.uploadToS3(urlExtMedium, mediumBuffer)
+        ])
+        .then((url) => {
+          Media.updatePGid([responseObject.url_med, responseObject.url_large], responseObject.id) // urlsArr initiated above
+          .then(() => {
+            res.status(201).json(responseObject);
+          })
+          .catch((err) => {
+            console.log('error updating URLs to PG db', err);
+          });
+        })
+        .catch((err) => {
+          console.log('error uploading images to s3 db', err)
+        });
+      })
+      .catch((err) => {
+        console.log('Error uploading metaData to PostgreSQL', err);
+      });
+
+    })
+    .catch( err => {
+      // check the type of error that was caught and display proper message
+      console.error(`Error resizing photo: ${err}`)
+      reject(`Error resizing photo: ${err}`)
+    })
+  }
+
   //parse data to separate photodata from photo
   // var photo = separateData(req.body);
   // console.log("Request files line 82 of media controllers: ", req.file);
@@ -83,49 +112,5 @@ exports.uploadPhoto = function (req, res) {
   // console.log("resizedPhotos object-boolean line 86 of media controllers: ", !!resizedPhotos);
   // update PG upload object to include small photo buffer
   // photo.PGupload.url_small = resizedPhotos.small;
-  var responseObject = {
-    id: null,
-    user_id: 5,
-    url_small: '', //resizedPhotos.small,
-    url_med: '',
-    url_large: '',
-    title: '', // photo.PGupload.title,
-    description: 'GOT IT' // photo.PGupload.description
-  };
-  Media.uploadToPG(req.body)
-  .then((id) => {
-    // console.log("Id returned from uploadToPG function line 100 of media controllers: ", id.rows[0].id);
-    responseObject.id = id.rows[0].id;
-    var urlExtLarge = responseObject.id + 'large';
-    // var urlExtMedium = responseObject.id + 'medium';
-    // responseObject.url_med = ('http://d14shq3s3khz77.cloudfront.net/' + urlExtMedium);
-    responseObject.url_large = ('http://d14shq3s3khz77.cloudfront.net/' + urlExtLarge);
-
-    new Promise.all(
-      [Media.uploadToS3(urlExtLarge, req.file.buffer)] //,
-      // Media.uploadToS3(urlExtMedium, resizedPhotos.medium)
-    )
-    .then((url) => {
-      Media.updatePGid([responseObject.url_med, responseObject.url_large], responseObject.id) // urlsArr initiated above
-      .then(() => {
-        res.status(201).json(responseObject);
-      })
-      .catch((err) => {
-        console.log('error updating URLs to PG db', err);
-      });
-    })
-    .catch((err) => {
-      console.log('error uploading images to s3 db', err)
-    });
-  })
-  .catch((err) => {
-    console.log('Error uploading metaData to PostgreSQL', err);
-  });
 
 };
-
-
-
-
-
-
