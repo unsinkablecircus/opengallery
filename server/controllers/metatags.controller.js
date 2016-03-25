aeb231922a41e0bb63f482efa27da23807ee487c
John Domingo <johnwill.domingo@gmail.com>
2016-03-25 02:14:22 -0700

    Define GET request handler with query parameters

--- server/controllers/metatags.controller.js
+++ server/controllers/metatags.controller.js
@@ -1,12 +1,30 @@
 const Meta = require('../models/metatags.model')
 
-exports.addTags = (req, res) => {
+/*-----------------------*/
+/*     GET /api/meta     */
+/*-----------------------*/
+exports.searchTags = (req, res) => {
+  // URL + '?tags=' + tags.join('?tags=')
+  Meta.fetch(req.query.tags)
+  .then( data => {
+    res.status(200).json(data)
+  })
+  .catch( err => {
+    console.error(`[Error] Failed to query meta tags in PG: ${err}`)
+    res.status(404).send(`[Error] Failed to query meta tags in PG: ${err}`)
+  })
+}
+
+/*------------------------*/
+/*     POST /api/meta     */
+/*------------------------*/
+exports.createTags = (req, res) => {
   Meta.insert(req.body.tags)
   .then( data => {
     res.status(201).json(data)
   })
   .catch( err => {
-    console.error(`[Error] Could not insert meta tags to PG: ${err}`)
-    res.status(400).send(`[Error] Could not insert meta tags to PG: ${err}`)
+    console.error(`[Error] Failed to insert meta tags to PG: ${err}`)
+    res.status(400).send(`[Error] Failed to insert meta tags to PG: ${err}`)
   })
 }
