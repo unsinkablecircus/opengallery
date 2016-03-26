2a6b9566bc6ba08d482721f4fef84366517169b9
John Domingo <johnwill.domingo@gmail.com>
2016-03-25 21:13:39 -0700

    Retrieve artist media as PSQL-formatted JSON

---
+++ server/models/user.model.js
@@ -0,0 +1,23 @@
+const pg = require('../db/database')
+const Promise = require('bluebird')
+
+const util = require('../config/helpers')
+const { isInt } = util
+
+exports.fetch = (user, artist) => {
+  if (isInt(user) && isInt(artist)) {
+    return pg.raw(
+      `SELECT array_to_json(array_agg(row_to_json(d))) as data
+      FROM (
+        SELECT m.id as media_id, m.title, m.media, m.description, m.width, m.height, m.url_small as url_sm, m.url_medium as url_md, m.url_large as url_lg
+        FROM media m
+        INNER JOIN users u
+        ON (m.user_id = u.id)
+        WHERE m.user_id = ${artist}
+        ORDER BY m.id
+      ) d`
+    )
+  } else {
+    return Promise.reject(`User ID must be integer, not ${typeof user}`)
+  }
+}
