7397277685dd19a294ad5de55ffcadc7397fffa2
John Domingo <johnwill.domingo@gmail.com>
2016-03-25 21:15:31 -0700

    Nest feedback data in each media JSON object

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -9,7 +9,16 @@ exports.fetch = (user, artist) => {
     return pg.raw(
       `SELECT array_to_json(array_agg(row_to_json(d))) as data
       FROM (
-        SELECT m.id as media_id, m.title, m.media, m.description, m.width, m.height, m.url_small as url_sm, m.url_medium as url_md, m.url_large as url_lg
+        SELECT
+          m.id as media_id, m.title, m.media, m.description, m.width, m.height, m.url_small as url_sm, m.url_medium as url_md, m.url_large as url_lg,
+          (
+            SELECT row_to_json(a)
+            FROM (
+              SELECT u.id, u.username, u.avatar_url
+              FROM users u
+              WHERE u.id = m.user_id
+            ) a
+          ) as artist
         FROM media m
         INNER JOIN users u
         ON (m.user_id = u.id)
