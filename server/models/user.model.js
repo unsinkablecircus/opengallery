c82e468ae21259afd802dd2a9275d8b5381b23e5
John Domingo <johnwill.domingo@gmail.com>
2016-03-26 11:05:02 -0700

    Factor out total record count for pagination

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,7 @@ const Promise = require('bluebird')
 const util = require('../config/helpers')
 const { isInt } = util
 
-exports.fetch = ({ user = 0, artist, page = 0 }) => {
+exports.fetch = ({ artist, user = 0, page = 0 }) => {
   if (isInt(user) && artist && isInt(page)) {
     return pg.raw(`
       SELECT array_to_json(array_agg(row_to_json(a))) AS artist
@@ -12,7 +12,7 @@ exports.fetch = ({ user = 0, artist, page = 0 }) => {
         SELECT
           u.id, u.username, u.name, u.email, u.facebook_url as facebook, u.twitter_url as twitter, u.avatar_url as avatar, u.media, u.about, u.website
         FROM users u
-        WHERE u.username = '${artist}'
+        WHERE u.username = '${ artist }'
       ) a;
 
       SELECT array_to_json(array_agg(row_to_json(d))) AS data
@@ -29,7 +29,7 @@ exports.fetch = ({ user = 0, artist, page = 0 }) => {
           ) AS artist,
           (
             SELECT array_agg(tags) AS tags
-            FROM (f
+            FROM (
               SELECT t.tag_text AS tags
               FROM media_tags mt
                 INNER JOIN tags t
@@ -41,27 +41,32 @@ exports.fetch = ({ user = 0, artist, page = 0 }) => {
             SELECT mh.hashtag_id
             FROM media_hashtags mh
             WHERE mh.media_id = m.id
-            AND mh.user_id = ${user}
+            AND mh.user_id = ${ user }
             LIMIT 1
           ) AS user_feedback_id,
           (
             SELECT array_to_json(array_agg(row_to_json(f)))
             FROM (
-              SELECT h.id, h.hashtag_text AS impression, mht.total AS count
+              SELECT h.id, h.hashtag_text AS word, mht.total AS count
               FROM media_hashtag_totals mht
                 INNER JOIN hashtags h
                 ON (mht.hashtag_id = h.id)
               WHERE mht.media_id = m.id
             ) f
-          ) AS feedback,
-        COUNT(*) OVER() AS total_data
+          ) AS feedback
         FROM media m
           INNER JOIN users u
           ON (m.user_id = u.id)
-        WHERE m.user_id = u.id AND u.username = '${artist}'
+        WHERE m.user_id = u.id AND u.username = '${ artist }'
         ORDER BY m.created_at
-        OFFSET ${18 * page} LIMIT 18
+        OFFSET ${ 18 * page } LIMIT 18
       ) d;
+
+      SELECT COUNT(*) as total_records
+      FROM media m
+        INNER JOIN users u
+        ON (m.user_id = u.id)
+      WHERE m.user_id = u.id AND u.username = '${ artist }';
     `)
   } else {
     return Promise.reject(`User ID must be number, not ${typeof parseInt(page)}`)
