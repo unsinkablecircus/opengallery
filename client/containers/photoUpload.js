033841fcb68c03ab6ddeeca239d41051ed5f04ae
Megan Ring <meganbethring@gmail.com>
2016-03-25 23:09:32 -0700

    Clean up photoUpload component

--- client/containers/photoUpload.js
+++ client/containers/photoUpload.js
@@ -4,10 +4,7 @@ import * as photo from '../actions/upload'
 
 import { toggleUpload } from '../actions/upload'
 
-//import other actions from actions folder
-
 const mapStateToProps = (state) => {
-  //update props with relevent states
   return {
     currentUser: state.user.id,
     currentPhotoIdToUpdateData: state.status.currentPhotoIdToUpdateData,
@@ -21,26 +18,18 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     onPhotoDrop: (media) => {
-      // console.log("Inside onPhotoDrop function in photoUpload container with photo: ", media, user);
       dispatch(photo.selectPhoto(media));
     },
     onOpenClick: () => {
-      // console.log("Inside onOpenClick function in photoUpload container with photo: ", photo);
       dispatch(photo.toggleDropWindow());
-      // this.refs.dropzone.open(); // also from dropZone example
     },
     onUploadSuccess: (response) => {
       dispatch(photo.uploadSuccess(response));
-    },
-    onUploadCancel: () => {
-      // console.log("Inside onUploadCancel function in photoUpload container");
-      dispatch(photo.uploadCancel());
     },
     onUploadFailure: (error) => {
       dispatch(photo.uploadError(error));
     },
     onUploadButtonClick: (data, media) => {
-      console.log("Inside onUploadButtonClick function");
       dispatch(photo.uploadPhoto(data, media));
     },
     closeUploadModal: () => {
