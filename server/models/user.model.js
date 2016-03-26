072cb6a84172039a8528560028e1e5b569e705b3
John Domingo <johnwill.domingo@gmail.com>
2016-03-25 22:23:47 -0700

    Add pagination feature

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,13 +4,13 @@ const Promise = require('bluebird')
 const util = require('../config/helpers')
 const { isInt } = util
 
-exports.fetch = (user, artist) => {
-  if (isInt(user) && isInt(artist)) {
+exports.fetch = ({ user, artist, page }) => {
+  if (isInt(user) && isInt(artist) && isInt(page)) {
     return pg.raw(
-      `SELECT array_to_json(array_agg(row_to_json(d))) as data
+      `SELECT array_to_json(array_agg(row_to_json(d))) AS data
       FROM (
         SELECT
-          m.id as media_id, m.title, m.media, m.description, m.width, m.height, m.url_small as url_sm, m.url_medium as url_md, m.url_large as url_lg,
+          m.id AS media_id, m.title, m.media, m.description, m.width, m.height, m.url_small AS url_sm, m.url_medium AS url_md, m.url_large AS url_lg,
           (
             SELECT row_to_json(a)
             FROM (
@@ -18,14 +18,14 @@ exports.fetch = (user, artist) => {
               FROM users u
               WHERE u.id = m.user_id
             ) a
-          ) as artist,
+          ) AS artist,
           (
-            SELECT array_agg(tags) as tags
+            SELECT array_agg(tags) AS tags
             FROM (
-              SELECT t.tag_text as tags
+              SELECT t.tag_text AS tags
               FROM media_tags mt
-              INNER JOIN tags t
-              ON (mt.tag_id = t.id)
+                INNER JOIN tags t
+                ON (mt.tag_id = t.id)
               WHERE mt.media_id = m.id
             ) tags
           ),
@@ -35,25 +35,27 @@ exports.fetch = (user, artist) => {
             WHERE mh.media_id = m.id
             AND mh.user_id = ${user}
             LIMIT 1
-          ) as user_feedback_id,
+          ) AS user_feedback_id,
           (
             SELECT array_to_json(array_agg(row_to_json(f)))
             FROM (
-              SELECT h.id, h.hashtag_text as impression, mht.total as count
+              SELECT h.id, h.hashtag_text AS impression, mht.total AS count
               FROM media_hashtag_totals mht
-              INNER JOIN hashtags h
-              ON (mht.hashtag_id = h.id)
+                INNER JOIN hashtags h
+                ON (mht.hashtag_id = h.id)
               WHERE mht.media_id = m.id
             ) f
-          ) as feedback
+          ) AS feedback,
+        COUNT(*) OVER() AS total_data
         FROM media m
-        INNER JOIN users u
-        ON (m.user_id = u.id)
+          INNER JOIN users u
+          ON (m.user_id = u.id)
         WHERE m.user_id = ${artist}
-        ORDER BY m.id
+        ORDER BY m.created_at
+        OFFSET ${18 * page} LIMIT 18
       ) d`
     )
   } else {
-    return Promise.reject(`User ID must be integer, not ${typeof user}`)
+    return Promise.reject(`User ID must be number, not ${typeof parseInt(page)}`)
   }
 }
