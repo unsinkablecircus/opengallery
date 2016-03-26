c1dd98efa60734f71fcfe99cf76788df5e7be606
John Domingo <johnwill.domingo@gmail.com>
2016-03-25 21:20:35 -0700

    Include user's feedback to show last impression

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,6 +12,12 @@ exports.fetch = (user, artist) => {
         SELECT
           m.id as media_id, m.title, m.media, m.description, m.width, m.height, m.url_small as url_sm, m.url_medium as url_md, m.url_large as url_lg,
           (
+            SELECT mh.hashtag_id
+            FROM media_hashtags mh
+            WHERE mh.media_id = m.id
+            AND mh.user_id = ${artist}
+          ) as user_feedback_id,
+          (
             SELECT row_to_json(a)
             FROM (
               SELECT u.id, u.username, u.avatar_url
