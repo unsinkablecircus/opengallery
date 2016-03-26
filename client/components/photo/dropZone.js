cac9a202f281e63019ccf65ae0ceea0a48b48db0
Megan Ring <meganbethring@gmail.com>
2016-03-25 17:44:07 -0700

    Comment out uploading status bar in dropZone component

--- client/components/photo/dropZone.js
+++ client/components/photo/dropZone.js
@@ -9,28 +9,28 @@ const DropZone = ({
   onOpenClick,
   currentFileUploading
 }) => {
-  console.log('currentfileuploading in dropzone', currentFileUploading)
+  // console.log('currentfileuploading in dropzone', currentFileUploading)
   const actions = [
     
   ];
-  // console.log('inside dropzone and the onUploadCancel is: ', onUploadCancel);
-  // onUploadCancel();
-  // console.log('inside dropzone and the currentFileUploading is: ', currentFileUploading[0]);
+
+  let photoObject = {};
+
   return (
     <div>
       <FlatButton label="Open" primary={true} type="button" onTouchTap={ onOpenClick } />
       <FlatButton label="Cancel" primary={true} type="button" onTouchTap={ onUploadCancel } />
-      <Dropzone onDrop={ (photo) => { onPhotoDrop(photo, currentUser) } } >
+      <Dropzone onDrop={ onPhotoDrop } >
         <div>Click inside or drop file to upload.</div>
       </Dropzone>
-      {currentFileUploading.length ? <div>
+      { /*currentFileUploading.length ? <div>
       <h2>Uploading {currentFileUploading.length} file...</h2>
       <div>
       {
         currentFileUploading.map((file, index) => <img key={index} src={file.preview} />)
       }
       </div>
-      </div> : null}
+      </div> : null */}
     </div>
   );
 }
