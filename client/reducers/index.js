49782b5154867456cd7f5207fa2df7ed91f9e0f7
Daniel Wong <danielwong2268@gmail.com>
2016-03-28 12:23:53 -0700

    Refactor the user page such that it can display information depending on artist vs. user Also add an artist reducer, which generates an artist state

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,19 +1,18 @@
-import { combineReducers } from 'redux';
-import {reducer as formReducer} from 'redux-form';
+import { combineReducers } from 'redux'
 import auth from './auth'
 // import grid from './grid';
 import media from './media'
 import status from './status'
 import view from './view'
 import user from './user'
-import { routerReducer } from 'react-router-redux';
-
+import artist from './artist'
+import { routerReducer } from 'react-router-redux'
 
 
 const reducers = combineReducers({
   auth,
-  form: formReducer,
   user,
+  artist,
   status,
   view,
   media,
