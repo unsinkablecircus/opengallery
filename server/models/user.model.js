48a1340d6f23694c154383fc5c76797b9ce505e4
John Domingo <johnwill.domingo@gmail.com>
2016-03-28 11:13:57 -0700

    Factor out reusable SQL snippets into helper

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -7,28 +7,18 @@ const { isInt } = query
 exports.fetch = ({ artist, user = 0, page = 0 }) => {
   if (isInt(user) && artist && isInt(page)) {
     return pg.raw(`
-      SELECT array_to_json(array_agg(row_to_json(a))) AS artist
-      FROM (
-        SELECT
-          u.id, u.username, u.name, u.email, u.facebook_url as facebook, u.twitter_url as twitter, u.avatar_url as avatar, u.media, u.about, u.website
-        FROM users u
-        WHERE u.username = '${ artist }'
-      ) a;
+      ${ query.artist(artist) }
 
       SELECT array_to_json(array_agg(row_to_json(d))) AS data
       FROM (
         SELECT ${ query.media(user) }
-        FROM ${ query.artist }
+        FROM ${ query.users }
         WHERE m.user_id = u.id AND u.username = '${ artist }'
         ORDER BY m.created_at
         OFFSET ${ 18 * page } LIMIT 18
       ) d;
 
-      SELECT COUNT(*) as total_records
-      FROM media m
-        INNER JOIN users u
-        ON (m.user_id = u.id)
-      WHERE m.user_id = u.id AND u.username = '${ artist }';
+      ${ query.total_artist(artist) }
     `)
   } else {
     return Promise.reject(`User ID must be number, not ${typeof parseInt(page)}`)
