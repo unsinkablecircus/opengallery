0432bc9dcfe88cec8515c6424cf6b3febfc41510
John Domingo <johnwill.domingo@gmail.com>
2016-03-29 14:59:28 -0700

    Add actions to update feedback on client side

--- client/actions/wordmap.actions.js
+++ client/actions/wordmap.actions.js
@@ -1,14 +1,34 @@
 export const SHOW_WORDMAP = 'SHOW_WORDMAP'
 export const HIDE_WORDMAP = 'HIDE_WORDMAP'
 
-export function show() {
+export function show () {
   return {
     type: SHOW_WORDMAP
   }
 }
 
-export function hide() {
+export function hide () {
   return {
     type: HIDE_WORDMAP
+  }
+}
+
+export function add ({ word, media }) {
+  return {
+    type: ADD_TO_WORDMAP,
+    payload: word,
+    meta: {
+      media
+    }
+  }
+}
+
+export function update ({ word, media }) {
+  return {
+    type: UPDATE_WORDMAP,
+    payload: word,
+    meta: {
+      media
+    }
   }
 }
