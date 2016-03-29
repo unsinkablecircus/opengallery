dff0dafdef8a2f5712ecd207d9392153240c697a
Megan Ring <meganbethring@gmail.com>
2016-03-28 20:35:56 -0700

    Initialize insert function to media_tags table

---
+++ server/models/media_metaTags.js
@@ -0,0 +1,29 @@
+const pg = require('../db/database');
+
+exports.insert = tags => {
+  if (Array.isArray(tags)) {
+    return pg.raw(`
+      BEGIN;
+      LOCK TABLE tags IN SHARE ROW EXCLUSIVE MODE;
+
+      WITH meta_tags (tag_text) AS (
+        VALUES ${
+          tags.map(t => `('${t}')`).join(',')
+        }
+      )
+      INSERT INTO tags (tag_text)
+      SELECT m.tag_text FROM meta_tags m
+      WHERE NOT EXISTS (
+        SELECT 1 FROM tags t
+        WHERE t.tag_text = m.tag_text
+      );
+
+      SELECT * FROM tags
+      WHERE tag_text = ANY ('{${ tags.join(',') }}'::text[]);
+
+      COMMIT;
+    `)
+  } else {
+    return Promise.reject(`Metatags must be posted as array, not as ${typeof tags}`)
+  }
+}
