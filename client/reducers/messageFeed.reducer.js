c4a3966d44bc5b221f189800183a4fc92cd927e9
Daniel Wong <danielwong2268@gmail.com>
2016-03-28 17:04:25 -0700

    Set up some boiler plate code for messageFeed feature. This includes: messageFeed reducer messageFeed presentational component messageFeed container messageFeed actions

---
+++ client/reducers/messageFeed.reducer.js
@@ -0,0 +1,22 @@
+import { initialState } from '../../test/initialState'
+
+let prevState = localStorage['my-save-key'] ? JSON.parse(localStorage['my-save-key']) : undefined;
+const startingState = prevState ? prevState.messageFeed : initialState.messageFeed;
+
+
+
+const messageFeedReducer = (state=startingState, action) => {
+
+  switch (action.type) {
+    case 'SUBMIT_MESSAGE':
+      // this should be called when you send in a message, so that it 
+      // can be updated in real time.
+      return Objec.assign({}, state, action.paylaod)
+    case 'FETCH_MESSAGES':
+      // this should be called when we get the messages back from the server
+      return Object.assign({}, state, action.payload)
+    default:
+      return state;
+
+  }
+}
