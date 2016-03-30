fda64904680f55f99ab612e9b151688a4b6accfb
John Domingo <johnwill.domingo@gmail.com>
2016-03-29 21:26:44 -0700

    Refactor data and error responses sent to user

--- server/controllers/feedback.js
+++ server/controllers/feedback.js
@@ -1,20 +1,18 @@
 var media_hashTags = require('../models/media_hashTags');
 
 module.exports = {
-
   submitFeedback: (req, res, body) => {
     var userId = req.body.userId;
     var mediaId = req.body.mediaId;
     var feedback = req.body.feedback.toLowerCase();
 
     media_hashTags.submitFeedback(userId, mediaId, feedback)
-    .then( (data) => {
-      console.log('data', data.rows[0]);
-      res.send({mediaHashtagTotal: data.rows[0]});
+    .then( data => {
+      res.status(201).json(data.rows[0]);
     })
-    .catch ( (err) => {
-      console.log('err', err);
-      res.status(500).send({err: err})
+    .catch ( err => {
+      console.error(`[Error] Could not post user feedback: ${err}`);
+      res.status(500).json(`[Error] Could not post user feedback: ${err}`)
     })
   }
 }
