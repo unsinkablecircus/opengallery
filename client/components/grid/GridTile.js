d70e8a268583c8eb8fe2e97cd1223b491d4454ee
Megan Ring <meganbethring@gmail.com>
2016-03-30 16:09:41 -0700

    Initialize delete button on conditional of if delete photos button has been clicked

--- client/components/grid/GridTile.js
+++ client/components/grid/GridTile.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import IconButton from 'material-ui/lib/icon-button'
+import DeleteIcon from 'material-ui/lib/svg-icons/action/delete'
 
 const GridTile = ({ tile, img, toggleGallery }) => {
+  let button;
+  if ( deleteMode ) {
+    button =  <IconButton>
+                <DeleteIcon>
+              </IconButton>
+  }
   return (
     <div
       className='tile-component'
       onClick={ () => {toggleGallery(tile) }}
       style={{backgroundImage: 'url(' + img + ')'}}
     >
+    {button}
     </div>
   )
 }
