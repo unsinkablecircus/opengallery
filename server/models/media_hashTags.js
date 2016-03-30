7796c1cad3fcc7c4de7d91cc2bb32fe4976c773f
John Domingo <johnwill.domingo@gmail.com>
2016-03-29 21:47:41 -0700

    Clean up console log

--- server/models/media_hashTags.js
+++ server/models/media_hashTags.js
@@ -2,22 +2,21 @@ const db = require('./../db/database')
 
 module.exports = {
   submitFeedback: (userId, mediaId, feedback) => {
-    console.log('info', userId, mediaId, feedback);
     return (
       db.raw(`
         WITH
           UserMedia AS (
-            SELECT * FROM media_hashtags 
+            SELECT * FROM media_hashtags
               WHERE (
                 user_id = ${userId} AND media_id = ${mediaId}
               )
             )
           UPDATE media_hashtag_totals
             SET total = total - 1
-            WHERE media_id = ${mediaId} AND hashtag_id = (SELECT hashtag_id FROM UserMedia)  
-            returning *;
-            
-        WITH 
+            WHERE media_id = ${mediaId} AND hashtag_id = (SELECT hashtag_id FROM UserMedia)
+            RETURNING *;
+
+        WITH
         new_row AS(
           INSERT INTO hashtags (hashtag_text)
           SELECT '${feedback}'
@@ -29,7 +28,7 @@ module.exports = {
           UNION
           SELECT * FROM hashtags WHERE hashtag_text='${feedback}'
         ),
-        upsert AS ( 
+        upsert AS (
           UPDATE media_hashtags
               SET hashtag_id = (SELECT id FROM combine)
           WHERE user_id = ${userId} AND media_id=${mediaId}
@@ -76,21 +75,11 @@ module.exports = {
   Insert an entry into media media_hashtags, if they haven't said anything before
   Insert/Upate the media_hashtag_totals
 
-  Problem is that there is no control in the media_hashtag_totals. With the current implementation, the user can only 
+  Problem is that there is no control in the media_hashtag_totals. With the current implementation, the user can only
   comment once on a picture
-  So, I need to 1. check to see if the user has commented on this before. 
+  So, I need to 1. check to see if the user has commented on this before.
     If so, find what he commented on before, decrement that value
     Increment the total count, if it exists in media_hashtag_totals
     If it does not exist, create a row in that table where the count is 1.
 
 */
-
-
-
-
-
-
-
-
-
-
