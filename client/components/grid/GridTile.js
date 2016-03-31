1513d644a81b7608b8094c7605ef59ff728ce6f2
Megan Ring <meganbethring@gmail.com>
2016-03-30 19:40:12 -0700

    Inherit deleteMode and addPhotoToBeDeleted functions from parent component; create on touchTap function to use deletePhoto function

--- client/components/grid/GridTile.js
+++ client/components/grid/GridTile.js
@@ -2,10 +2,10 @@ import React from 'react'
 import IconButton from 'material-ui/lib/icon-button'
 import DeleteIcon from 'material-ui/lib/svg-icons/action/delete'
 
-const GridTile = ({ tile, img, toggleGallery, deleteMode }) => {
+const GridTile = ({ tile, img, toggleGallery, deleteMode, addPhotoToBeDeleted }) => {
   let button;
   if ( deleteMode ) {
-    button =  <IconButton onTouchTap={ () => }>
+    button =  <IconButton onTouchTap={ (photo) => addPhotoToBeDeleted(photo) }>
                 <DeleteIcon>
               </IconButton>
   }
