bd3f68e91f167c3a8d294da5040dff181620c329
John Domingo <johnwill.domingo@gmail.com>
2016-03-31 15:35:36 -0700

    Add input placeholder based on user's last word

--- client/components/wordmap/Feedback.js
+++ client/components/wordmap/Feedback.js
@@ -2,14 +2,24 @@ import React from 'react'
 
 export default class Feedback extends React.Component {
   render () {
-    const { user, data, submit } = this.props
+    const { user, data, feedback, idx, submit } = this.props
+
+    const word = feedback.reduce((acc, word) =>
+      acc || word.id == idx && word.tag, false)
+
     return (
       <div id="feedback-component">
-        <form onSubmit={ (e) => {
+        <form className="feedback-form" onSubmit={ (e) => {
           e.preventDefault()
           submit(user, data, this.refs.feedbackInfo.value)
+          this.refs.feedbackInfo.value = ''
         }}>
-          <input type="text" className="feedback-input" ref='feedbackInfo'/>
+          <input
+            type="text"
+            ref='feedbackInfo'
+            placeholder={ word || 'feedback' }
+            className="feedback-input"
+          />
         </form>
       </div>
     )
