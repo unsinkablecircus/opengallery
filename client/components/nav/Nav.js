d15803618b8b59c6b62aea97764f5d93d18562c1
John Domingo <johnwill.domingo@gmail.com>
2016-04-01 14:20:24 -0700

    Refactor Nav component without material-ui

--- client/components/nav/Nav.js
+++ client/components/nav/Nav.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 
+import Logo from './Logo'
+
 import AppBar from 'material-ui/lib/app-bar'
 import CreateIcon from 'material-ui/lib/svg-icons/image/palette'
 import FlatButton from 'material-ui/lib/flat-button'
@@ -46,59 +48,46 @@ let Nav = ({
     }
   }
 
+  const style = {
+    backgroundImage: 'url(assets/icons/palette.svg)'
+  }
+
   let searchInput;
 
   return (
-    <MuiThemeProvider muiTheme={darkMuiTheme}>
-
-      <AppBar
-        id="nav-component"
-        style={styles.nav}
-        iconElementLeft={
-          <div>
-            <Link to={'/'}><FlatButton
-              label='Open Gallery'
-              backgroundColor='#0097a7'
-              style={styles.title}
-            /></Link>
-            <IconButton tooltip='Create' onClick={ onToggleUpload }>
-              <CreateIcon color='#303030'/>
-            </IconButton>
-            <IconButton 
-              tooltip='Discover'
-              onClick={ () => {handleConversationsIconClick(id)}}
-            >
-              <ProfileIcon color='#303030'/>
-            </IconButton>
-            <input
-              style={{borderRadius: '3px', height: '30px', width: '500px', marginLeft: '20px', transform: 'translateY(-5px)', fontSize:'20px'}}
-              ref={ (node) => {searchInput = node} }
-            />
-            <FlatButton
-              style={{'marginLeft': '10px', fontSize:'20px'}}
-              label='Search'
-              onTouchTap={ () => {onSearchSubmit(id, searchInput.value)} }
-            />
+    <nav id="nav-component">
+      <ul className="nav-list">
+        <li className="nav-item-logo">
+          <Logo/>
+        </li>
+        <li className="nav-item nav-item-search">
+          <input className="nav-item-input"
+            placeholder="Discover"
+            style={style}
+            ref={ (node) => {searchInput = node} }
+          />
+        </li>
+        <li className="nav-item">
+          <div className="nav-item-button">
+            Upload
           </div>
-        }
-        iconElementRight={
-          <IconMenu
-            iconButtonElement={
-              <IconButton><ProfileIcon/></IconButton>
-            }
-            targetOrigin={{horizontal: 'right', vertical: 'top'}}
-            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-          >
-            <Link to={`/profile/${username}`}>
-              <MenuItem
-                primaryText='Profile'
-              />
-            </Link>
-            <Link to={'/'}><MenuItem primaryText='Sign Out' onClick={ onLogoutClick }/></Link>
-          </IconMenu>
-        }
-      />
-   </MuiThemeProvider>
+          <div className="nav-item-button">
+            Inbox
+          </div>
+          <Link
+            to={`/profile/${username}`}
+            className="nav-item-button">
+            { username || 'Sign In'}
+          </Link>
+          {/*<Link
+            to={'/'}
+            onClick={ onLogoutClick }
+            className="nav-item-button">
+            Sign Out
+          </Link>*/}
+        </li>
+      </ul>
+    </nav>
   )
 }
 
