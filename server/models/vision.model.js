a4de63441969da6c1db3af2f2825a97b7cb5bc76
Megan Ring <meganbethring@gmail.com>
2016-04-01 14:40:46 -0700

    Refactor vision model to accept url parameter

--- server/models/vision.model.js
+++ server/models/vision.model.js
@@ -3,11 +3,10 @@ const Promise = require('bluebird')
 
 vision.init({ auth: process.env.API_GOO_VISION_SERVER_KEY })
 
-console.log("api key", process.env.API_GOO_VISION_SERVER_KEY)
-
 exports.analyze = (imageUrl) => {
+  console.log("inside googlevision analyze function", imageUrl);
   const req = new vision.Request({
-    image: new vision.Image({ url: '${imageUrl}' }),
+    image: new vision.Image({ url: {imageUrl} }),
     // image: new vision.Image(__dirname + '/../../test/server/circus.jpg'),
     features: [
       new vision.Feature('LABEL_DETECTION', 10),
@@ -17,7 +16,9 @@ exports.analyze = (imageUrl) => {
 
   return vision.annotate(req)
   .then( res => {
+    console.log("inside then statement");
     console.log(JSON.stringify(res.responses))
+    
     return res.responses
   })
   .catch( err => {
