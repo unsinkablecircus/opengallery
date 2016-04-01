65c44f57f1be996e75212612473e6963204b0919
Megan Ring <meganbethring@gmail.com>
2016-03-31 23:12:21 -0700

    Remve button from GridTile

--- client/components/grid/GridTile.js
+++ client/components/grid/GridTile.js
@@ -3,12 +3,7 @@ import IconButton from 'material-ui/lib/icon-button'
 import DeleteIcon from 'material-ui/lib/svg-icons/action/delete'
 
 const GridTile = ({ key, tile, img, toggleGallery, deleteMode, addPhotoToBeDeleted }) => {
-  let button;
-    if ( deleteMode ) {
-      console.log("EXTREME DELETE MODE!!!!!!!!!!!!!!")
-      button =  <IconButton icon={<DeleteIcon/>} 
-                            onTouchTap={ () => {console.log("Inside GridTile"); addPhotoToBeDeleted(key) }} />
-  }
+  
   return (
     <div
       className='tile-component'
