ba667ee013ccddea9c46f13c13b0d62abde1bebb
Evan Chen <evanwchen@gmail.com>
2016-04-06 21:53:22 -0700

    Fix typo on formData state

--- client/containers/user.js
+++ client/containers/user.js
@@ -14,7 +14,7 @@ const mapStateToProps = (state, ownProps) => {
     deleteMode: state.user.deleteMode,
     photosToBeDeleted: state.user.photosToBeDeleted,
     location: ownProps,
-    formData: state.form.EDIT_PROFILE_INFORMATION
+    formData: state.form.profileInformation
   }
 }
 
