fcb938f22e8bac8135428f845a64e40025215b19
Evan Chen <evanwchen@gmail.com>
2016-04-08 10:09:13 -0700

    Add 'http://' for Access-Control-Allow-Origin on middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -40,7 +40,7 @@ module.exports = function (app, express) {
   }
 
   app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'opengallery.io');
+    res.header('Access-Control-Allow-Origin', 'http://opengallery.io');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
