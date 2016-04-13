dbd9d408ca61b946de186856cb10258f3296182c
Evan Chen <evanwchen@gmail.com>
2016-04-13 16:34:34 -0700

    Add logs for debugging. Remove --watch from nodemon

--- server/models/media.js
+++ server/models/media.js
@@ -33,12 +33,15 @@ exports.uploadToS3 = function (photoId, photo) {
     ContentType: 'image/jpeg',
     Body: photo
   };
+  console.log('params: ', params);
   return new Promise(function(resolve, reject) {
+    console.log('uploading to S3...');
     s3.putObject(params, function(err, data) {
       if (err) {
         console.log('Error uploading to S3: ', err);
         reject(err);
       } else {
+        console.log('Uploaded to S3: ', data);
         resolve(data);
       }
     });
